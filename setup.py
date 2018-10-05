fba3e9a39d10ae5557ade570fe3b27ee55b4fcfd
Ingo Fischer <i.fischer@syseleven.de>
2018-10-05 13:34:59 +0200

    Update url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 PACKAGE = "icinga2api"
 NAME = "python-icinga2api"
 DESCRIPTION = "python icinga2 api "
-URL = "https://github.com/ifischer/python-icinga2api"
+URL = "https://github.com/syseleven/python-icinga2api"
 AUTHOR = __import__(PACKAGE).__author__
 AUTHOR_EMAIL = __import__(PACKAGE).__contact__
 VERSION = __import__(PACKAGE).__version__
